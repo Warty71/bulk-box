4f2d45b4119019182614c9fceea9f223ba97a117
Meris Lihic <meris.lihic@gmail.com>
2025-02-23 22:20:42 +0100

    feat: increased timeout to 30 seconds

--- lib/src/features/collection/data/datasources/remote/ygopro_api_datasource.dart
+++ lib/src/features/collection/data/datasources/remote/ygopro_api_datasource.dart
@@ -8,8 +8,8 @@ class YGOProApiDatasource {
       : _dio = Dio(
           BaseOptions(
             baseUrl: baseUrl,
-            connectTimeout: const Duration(seconds: 5),
-            receiveTimeout: const Duration(seconds: 3),
+            connectTimeout: const Duration(seconds: 30),
+            receiveTimeout: const Duration(seconds: 30),
           ),
         );
 
