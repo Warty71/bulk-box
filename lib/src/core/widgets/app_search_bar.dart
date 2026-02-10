3a5571637e4951f7b6199ccc33898df0da58fbc2
KerimKadric <kerim.kadric.22@size.ba>
2026-02-10 16:27:02 +0100

    Made widget for SearchBar

---
+++ lib/src/core/widgets/app_search_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:bulk_box/src/core/constants/dimensions.dart';
+
+class AppSearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final ValueChanged<String>? onChanged;
+  final VoidCallback? onClear;
+
+  const AppSearchBar({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.onChanged,
+    this.onClear,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        borderRadius: BorderRadius.circular(Dimensions.radiusLg),
+        border: Border.all(
+          color: theme.dividerColor,
+        ),
+
+      
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          hintText: hintText,
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: controller.text.isNotEmpty
+              ? IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    controller.clear();
+                    onClear?.call();
+                  },
+                )
+              : null,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: Dimensions.sm,
+            horizontal: Dimensions.md,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/features/collection/presentation/widgets/toolbar/collection_search_bar.dart
+++ lib/src/features/collection/presentation/widgets/toolbar/collection_search_bar.dart
@@ -1,6 +1,9 @@
-import 'package:bulk_box/src/features/collection/presentation/cubit/collection_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bulk_box/src/core/constants/dimensions.dart';
+import 'package:bulk_box/src/core/widgets/app_search_bar.dart';
+import 'package:bulk_box/src/features/collection/presentation/cubit/collection_cubit.dart';
 
 class CollectionSearchBar extends StatefulWidget {
   const CollectionSearchBar({super.key});
@@ -11,79 +14,27 @@ class CollectionSearchBar extends StatefulWidget {
 
 class _CollectionSearchBarState extends State<CollectionSearchBar> {
   final _controller = TextEditingController();
-  final _focusNode = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-
-    // auto focus when shown
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _focusNode.requestFocus();
-    });
-  }
 
   @override
   void dispose() {
     _controller.dispose();
-    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Material(
-      elevation: 3,
-      borderRadius: BorderRadius.circular(14),
-      color: theme.colorScheme.surface,
-      child: TextField(
+    return Padding(
+      padding: const EdgeInsets.all(Dimensions.md),
+      child: AppSearchBar(
         controller: _controller,
-        focusNode: _focusNode,
-        onChanged: (query) => context.read<CollectionCubit>().search(query),
-        decoration: InputDecoration(
-          hintText: 'Search your collection...',
-          prefixIcon: const Icon(Icons.search),
-          suffixIcon: ValueListenableBuilder<TextEditingValue>(
-            valueListenable: _controller,
-            builder: (_, value, __) {
-              if (value.text.isEmpty) return const SizedBox.shrink();
-
-              return _SearchSuffixIcon(
-                controller: _controller,
-              );
-            },
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(14),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: theme.colorScheme.surface,
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-        ),
+        hintText: 'Search your collection...',
+        onChanged: (query) {
+          context.read<CollectionCubit>().search(query);
+        },
+        onClear: () {
+          context.read<CollectionCubit>().search('');
+        },
       ),
-    );
-  }
-}
-
-class _SearchSuffixIcon extends StatelessWidget {
-  const _SearchSuffixIcon({
-    required this.controller,
-  });
-
-  final TextEditingController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.close),
-      onPressed: () {
-        controller.clear();
-        context.read<CollectionCubit>().search('');
-      },
     );
   }
 }
